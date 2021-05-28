117e189f2bcda0f24b675877ab4a72daf801836c
Pedro <pcastillotorio@gmail.com>
2021-05-28 12:58:36 +0200

    Actualizada la vista de busqueda

--- src/app/componentes/busqueda/busqueda.component.ts
+++ src/app/componentes/busqueda/busqueda.component.ts
@@ -8,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class BusquedaComponent implements OnInit {
 
   constructor() { }
+  public dataSource: any[];
+
+  public headerTitle: string = "Resultados de su búsqueda";
+  public cssClass: string = 'e-list-template';
 
   ngOnInit(): void {
+    this.dataSource = [{
+      "titulo": "Colegio 1",
+      "descripcion": "descripcion colegio 1",
+      "direccion": "direccion colegio 1"
+    },
+    {
+      "titulo": "Colegio 2",
+      "descripcion": "descripcion colegio 2",
+      "direccion": "direccion colegio 2"
+    },
+    {
+      "titulo": "Hospital 1",
+      "descripcion": "descripcion hospital 1",
+      "direccion": "direccion hospital 1"
+    },
+    {
+      "titulo": "Hospital 2",
+      "descripcion": "descripcion hospital 2",
+      "direccion": "direccion hospital 2"
+    }]
   }
+
+  public onActionComplete(event: any){
+
+  }
+
 
   public busqueda(event: any){
     event.preventDefault();
