78ba00949c50f630ced6c838f7cd3303dd81cf34
juan sebastian quinayas guarin <sebastianquemasda@gmail.com>
2021-06-08 12:20:49 +0200

    modelo colegio y servicio

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Usuario} from 'src/app/modelo/usuarios';
+import { Colegio } from 'src/app/modelo/colegio';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -20,6 +21,12 @@ export class DataService {
   }
   public getUserId(){
     return this.httpClient.get(this.REST_API_SERVER);
+  }
+
+  public updateColegio(colegio: Colegio) {
+    const headers = { 'content-type': 'application/json' }
+    const body = JSON.stringify(colegio)
+    return this.httpClient.put(this.REST_API_SERVER + environment.crearUser, body, { 'headers': headers })
   }
 
   public addUser(usuario:Usuario){
